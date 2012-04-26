fe53285079cc33f443af39592d02b9fcd1dccc0e
Steve <wjruoxue@gmail.com>
2012-04-26 11:37:00 -0700

    add message receiver

---
+++ src/mobisocial/omnistanford/MessageReceiver.java
@@ -0,0 +1,40 @@
+package mobisocial.omnistanford;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import org.json.JSONObject;
+
+import mobisocial.socialkit.musubi.DbObj;
+import mobisocial.socialkit.musubi.Musubi;
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MessageReceiver extends BroadcastReceiver {
+	public static final String TAG = "MessageReceiver";
+	
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Uri objUri = intent.getParcelableExtra("objUri");
+        if (objUri == null) {
+            Log.i("WordPlayNotification", "No object found");
+            return;
+        }
+
+        Musubi musubi = Musubi.forIntent(context, intent);
+        DbObj obj = musubi.objForUri(objUri);
+//        if (obj.getSender().isOwned()) {
+//            return;
+//        }
+        
+        Log.i(TAG, "received message " + intent);
+
+        // Dont notify in Musubi
+        Bundle b = new Bundle();
+        b.putInt("notification", 0);
+        setResult(Activity.RESULT_OK, null, b);
+    }
+}
