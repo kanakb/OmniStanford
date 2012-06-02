39080f67de83d7952c6dc18d054362a02d870de0
Kanak Biscuitwala <kanak.b@hotmail.com>
2012-06-02 14:47:56 -0700

    Don't assume the location is better even if it's 5 minutes newer

--- src/mobisocial/omnistanford/service/LocationService.java
+++ src/mobisocial/omnistanford/service/LocationService.java
@@ -376,21 +376,17 @@ public class LocationService extends Service {
 
        // Check whether the new location fix is newer or older
        long timeDelta = location.getTime() - mCurrent.getTime();
-       boolean isSignificantlyNewer = timeDelta > MIN_TIME_SHORT;
        boolean isSignificantlyOlder = timeDelta < -MIN_TIME_SHORT;
        boolean isNewer = timeDelta > 0;
 
-       // If it's been more than two minutes since the current location, use the new location
-       // because the user has likely moved
-       if (isSignificantlyNewer) {
-           return true;
        // If the new location is more than two minutes older, it must be worse
-       } else if (isSignificantlyOlder) {
+       if (isSignificantlyOlder) {
            return false;
        }
 
        // Check whether the new location fix is more or less accurate
        int accuracyDelta = (int) (location.getAccuracy() - mCurrent.getAccuracy());
+       Log.d(TAG, location.getProvider() + " accuracy: " + location.getAccuracy());
        boolean isLessAccurate = accuracyDelta > 0;
        boolean isMoreAccurate = accuracyDelta < 0;
        boolean isSignificantlyLessAccurate = accuracyDelta > MIN_DISTANCE;
